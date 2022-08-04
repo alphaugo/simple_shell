0766eb776eed6b57d9780198617637fad6c80038
Theophilus <theophilusben2002@gmail.com>
2022-08-04 06:07:30 -0700

    perser to executor

---
+++ persers.c
@@ -0,0 +1,33 @@
+#include "header.h"
+
+/**
+ * perser - tokenizes the string
+ * @str: user input
+ * Return: pointer to array of tokens.
+ */
+
+char **perser(char *str)
+{
+    char **token;
+    char *tok;
+    unsigned int i;
+
+    token = malloc(sizeof(char) * BUFFER);
+    if (token == NULL)
+    {
+        errors(3);
+        exit(EXIT_FAILURE);
+    }
+
+    tok = strtok(str, "\n\t\r ");
+
+    i = 0;
+    while (tok != NULL)
+    {
+        token[i] = tok;
+        token = strtok(NULL, "\n\t\r ");
+        i++;
+    }
+    token[i] = NULL;
+    return (token);
+}
