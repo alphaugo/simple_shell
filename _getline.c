2bf71fd611215d05a7e8e21fa6dd85980232419d
Theophilus <theophilusben2002@gmail.com>
2022-08-04 05:32:50 -0700

    reads commands

---
+++ _getline.c
@@ -0,0 +1,27 @@
+#include "header.h"
+/**
+ * _getline - function that puts input from user to buffer
+ * 
+ * @fd: buffer for user input
+ * Return: buffer with input
+ */
+
+char *_getline(FILE *fd)
+{
+    char *line;
+    ssize_t read;
+    size_t len;
+
+    line = NULL;
+    len = 0;
+
+    read = getline(&line, &len, fd);
+    if (read == -1)
+    {
+        free(line);
+        exit(EXIT_SUCCESS);
+    }
+
+    return (line);
+
+}
