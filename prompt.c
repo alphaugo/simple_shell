66b76b2fbdd83bbaec90b3d54fc6791775e9441d
Theophilus <theophilusben2002@gmail.com>
2022-08-04 06:09:40 -0700

    generate command prompt

---
+++ prompt.c
@@ -0,0 +1,33 @@
+#include "header.h"
+
+/**
+ * @brief 
+ * prompt - function.
+ * 
+ * @param fd 
+ * @param buf 
+ */
+
+void prompt(int fd, struct stat buf)
+{
+    fstat(fd, &buf);
+
+    if (S_ISCHR(buf.st_mode))
+        _puts(PROMPT);
+}
+/**
+ * _puts - function
+ * 
+ * @str: parameter
+ * 
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+    unsigned int length;
+
+    length = _strlen(str);
+
+    write(STDOUT_FILENO, str, length);
+}
